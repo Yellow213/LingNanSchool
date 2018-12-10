222346094ae7b42438c062cbc0ffa351c30a0476
dingchw <dingchw@dist.com.cn>
2018-12-10 16:43:59 +0800

    --v0.0.0.0.0.0.0

---
+++ src/main/java/cn/edu/lingnan/service/BaseController.java
@@ -0,0 +1,8 @@
+package cn.edu.lingnan.service;
+
+/**
+ * @author ChiV3n
+ * @date 2018/12/10.
+ */
+public class BaseController {
+}
---
+++ src/test/java/cn/edu/lingnan/BaseTest.java
@@ -0,0 +1,8 @@
+package cn.edu.lingnan;
+
+/**
+ * @author ChiV3n
+ * @date 2018/12/10.
+ */
+public class BaseTest {
+}
